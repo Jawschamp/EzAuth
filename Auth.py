1d28bcf8ddc4d8b3ce05ccf9d275b9b99e64d3b1
Jaws <38185563+Jawschamp@users.noreply.github.com>
2021-10-01 01:42:49 -0700

    Add files via upload

--- Auth.py
+++ Auth.py
@@ -1,26 +1,28 @@
-import json
+#import json
 import requests
 
-#
+
 from base64 import b64encode
-from dotenv import load_dotenv
 
 
 id_secret = "ec684b8c687f479fadea3cb2ad83f5c6:e1f31c211f28413186262d37a13fc84d"
-code = input("Go to https://www.epicgames.com/id/api/redirect?clientId=ec684b8c687f479fadea3cb2ad83f5c6&responseType=code".format(id_secret))
+#code = input("Go to https://www.epicgames.com/id/api/redirect?clientId=ec684b8c687f479fadea3cb2ad83f5c6&responseType=code"
 
-def get_access_token():
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"basic {str(b64encode(id_secret.encode('utf-8')), 'utf-8')}"
-    }
+class AuthMethods():
+    def get_access_token(code):
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"basic {str(b64encode(id_secret.encode('utf-8')), 'utf-8')}"
+        }
 
-    body = {
-        "grant_type": "authorization_code",
-        "code": code
-    }
+        body = {
+            "grant_type": "authorization_code",
+            "code": code
+        }
 
-    req = requests.post("https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
-    headers=headers, data=body)
-    print(req.json())
-    return req.json()
+        req = requests.post("https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
+        headers=headers, data=body)
+        print(req.json())
+        return req.json()
+
+AuthMethods.get_access_token(code="ddc3671d11da420e9985e0c4147eb49c")
